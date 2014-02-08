f3a54d985e00bf23c1910e378efeefc95d0c5a58
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-08 12:54:18 +0100

    Remove no longer functional code

--- init.el
+++ init.el
@@ -13,11 +13,3 @@
 
 (eval-after-load "magit"
   '(define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
-
-(defun magit-just-amend ()
-  (interactive)
-  (save-window-excursion
-    (magit-with-refresh (shell-command "git --no-pager commit --amend --reuse-message=HEAD"))))
-
-(eval-after-load "magit"
-  '(define-key magit-status-mode-map (kbd "C-c C-a") 'magit-just-amend))
