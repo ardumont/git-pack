cbd6075266f5ba960106f8461fbaa637eb320dd0
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-04 18:36:06 +0100

    Define a git-pack-mode minor mode

--- init.el
+++ init.el
@@ -10,9 +10,6 @@
 (require 'git-gutter)
 (require 'magit)
 
-;; Magit
-(global-set-key (kbd "C-x g") 'magit-status)
-
 ;; full screen magit-status
 (defadvice magit-status (around magit-fullscreen activate)
   (window-configuration-to-register :magit-fullscreen)
@@ -28,15 +25,32 @@
 (eval-after-load "magit"
   '(define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
 
-;; Git Gutter
-(global-set-key (kbd "C-c g g") 'git-gutter:toggle)
-
-;; Jump to next/previous diff
-(global-set-key (kbd "C-c g p") 'git-gutter:previous-diff)
-(global-set-key (kbd "C-c g n") 'git-gutter:next-diff)
-(global-set-key (kbd "C-c g d") 'git-gutter:popup-diff)
-(global-set-key (kbd "C-c g r") 'git-gutter:revert-hunk)
+(defvar git-pack-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c g s") 'magit-status)
+    (define-key map (kbd "C-c g g") 'git-gutter:toggle)
+    (define-key map (kbd "C-c g p") 'git-gutter:previous-diff)
+    (define-key map (kbd "C-c g n") 'git-gutter:next-diff)
+    (define-key map (kbd "C-c g d") 'git-gutter:popup-diff)
+    (define-key map (kbd "C-c g r") 'git-gutter:revert-hunk)
+    map)
+  "Keymap for git-pack mode.")
 
 (global-git-gutter-mode)
+
+(define-minor-mode git-pack-mode
+  "Minor mode to consolidate git-pack extensions.
+
+\\{git-pack-mode-map}"
+  :lighter " GP"
+  :keymap git-pack-mode-map)
+
+(define-globalized-minor-mode global-git-pack-mode git-pack-mode git-pack-on)
+
+(defun git-pack-on ()
+  "Turn on `git-pack-mode'."
+  (git-pack-mode +1))
+
+(global-git-pack-mode)
 
 ;;; git-pack ends here
