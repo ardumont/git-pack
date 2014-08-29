f2a1719e1edaae0c28a921c7192822458a83fb82
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-08-29 17:08:41 +0200

    Doc - Improve comments

--- git-pack.el
+++ git-pack.el
@@ -12,20 +12,19 @@
 
 (require 'magit)
 
-;; full screen magit-status
 (defadvice magit-status (around magit-fullscreen activate)
+  "Keep the actual window disposition."
   (window-configuration-to-register :magit-fullscreen)
   ad-do-it
   (delete-other-windows))
 
 (defun magit-quit-session ()
-  "Restores the previous window configuration and kills the magit buffer"
+  "Restore the previous window configuration and kill the magit buffer."
   (interactive)
   (kill-buffer)
   (jump-to-register :magit-fullscreen))
 
-(eval-after-load "magit"
-  '(define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
+(eval-after-load "magit" '(define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
 
 ;; git-pack
 
