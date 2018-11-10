72ed10703384c1e18e23aea4203c133048f1260d
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-10 22:28:36 +0100

    Add magit-mode to dired-mode

--- git-pack.el
+++ git-pack.el
@@ -14,6 +14,7 @@
 
 (require 'git-gutter)
 (add-hook 'prog-mode-hook 'git-gutter-mode)
+(add-hook 'dired-mode-hook 'magit-mode)
 
 (custom-set-variables
  '(custom-set-variables '(magit-auto-revert-mode nil))
